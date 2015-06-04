1519e63a1bd5fec7f1a90e79aa42be3a9396640f
Bram De Smyter <bram@bubblefish.be>
2015-06-05 00:00:04 +0200

    Update DeleteCategory.php

--- src/Backend/Modules/Agenda/Actions/DeleteCategory.php
+++ src/Backend/Modules/Agenda/Actions/DeleteCategory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Backend\Modules\Agenda\Actions;
+namespace Backend\Modules\Team\Actions;
 
 /*
  * This file is part of Fork CMS.
@@ -12,12 +12,12 @@ namespace Backend\Modules\Agenda\Actions;
 use Backend\Core\Engine\Base\ActionDelete as BackendBaseActionDelete;
 use Backend\Core\Engine\Model as BackendModel;
 use Backend\Core\Engine\Language as BL;
-use Backend\Modules\Agenda\Engine\Model as BackendAgendaModel;
-
+use Backend\Modules\Team\Engine\Model as BackendTeamModel;
+ 
 /**
- * This action will delete a category
- *
+ * This action will delete a category.
  * @author Tim van Wolfswinkel <tim@webleads.nl>
+ * @author Bram De Smyter <bram@bubblefish.be>
  */
 class DeleteCategory extends BackendBaseActionDelete
 {
@@ -29,23 +29,23 @@ class DeleteCategory extends BackendBaseActionDelete
 		$this->id = $this->getParameter('id', 'int');
 
 		// does the item exist
-		if($this->id == null || !BackendAgendaModel::existsCategory($this->id)) {
-			$this->redirect(
-				BackendModel::createURLForAction('categories') . '&error=non-existing'
-			);
+		if($this->id !== null && BackendTeamModel::existsCategory($this->id))
+		{
+			$this->record = (array) BackendTeamModel::getCategory($this->id);
+
+			if(BackendTeamModel::deleteCategoryAllowed($this->id))
+			{
+				parent::execute();
+
+				// delete item
+				BackendTeamModel::deleteCategory($this->id);
+				BackendModel::triggerEvent($this->getModule(), 'after_delete_category', array('item' => $this->record));
+
+				// category was deleted, so redirect
+				$this->redirect(BackendModel::createURLForAction('categories') . '&report=deleted-category&var=' . urlencode($this->record['title']));
+			}
+			else $this->redirect(BackendModel::createURLForAction('categories') . '&error=delete-category-not-allowed&var=' . urlencode($this->record['title']));
 		}
-
-		// fetch the category
-		$this->record = (array) BackendAgendaModel::getCategory($this->id);
-
-		// delete item
-		BackendAgendaModel::deleteCategory($this->id);
-		BackendModel::triggerEvent($this->getModule(), 'after_delete_category', array('item' => $this->record));
-
-		// category was deleted, so redirect
-		$this->redirect(
-			BackendModel::createURLForAction('categories') . '&report=deleted-category&var=' .
-			urlencode($this->record['title'])
-		);
+		else $this->redirect(BackendModel::createURLForAction('categories') . '&error=non-existing');
 	}
 }
